a09e1b075bb430e9458127a2d562c3056e0e3b27
Vishnu kanhan <kanhanvishnu@gmail.com>
2024-02-06 05:07:14 +0530

    Added users info

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ func main() {
 	conferenceName := "Go conference"
 	const conferenceTicket = 50
 	var remainingTicket uint = 50
+	bookings := []string{}
 
 	fmt.Printf("Conference name is: %T, Remaining tickets is %T\n", conferenceName, conferenceTicket)
 
@@ -14,26 +15,36 @@ func main() {
 	fmt.Printf("We have %d conference ticket and %d remaining tickets!\n", conferenceTicket, remainingTicket)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket uint
-	// login page
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
+		// login page
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+		
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+		
+		fmt.Println("Enter the number of ticketes: ")
+		fmt.Scan(&userTicket)
+		
+		remainingTicket = remainingTicket - userTicket
+		
+		//var bookings [50]string
+		//bookings[0] = firstName + " " + lastName //array
+		bookings = append(bookings, firstName+" "+lastName)
+		
+		fmt.Printf("%v tickets are remaining for %v\n", remainingTicket, conferenceName)
+		fmt.Printf("Thank you %v %v for booking %v tickets! You will recieve a confirmation email at %v\n", firstName, lastName, userTicket, email)
+		
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
 
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
-
-	fmt.Println("Enter the number of ticketes: ")
-	fmt.Scan(&userTicket)
-
-	remainingTicket = remainingTicket - userTicket
-
-	fmt.Printf("Thank you %v %v for booking %v tickets! You will recieve a confirmation email at %v\n", firstName, lastName, userTicket, email)
-	fmt.Printf("%v tickets are remaining for %v", remainingTicket, conferenceName)
+	
 }
